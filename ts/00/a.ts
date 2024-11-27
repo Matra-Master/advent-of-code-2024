6fa417791adb3baf7730eec1896ca9095bde8ae9
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-11-27 09:42:26 -0300

    first commit
    Typescript init folder

---
+++ ts/00/a.ts
@@ -0,0 +1,16 @@
+import { ensureFile } from "@std/fs";
+
+//
+//PUT YOUR INPUTS ON A VARIABLE
+await ensureFile("input.txt");
+const input = Deno.readTextFileSync("example.txt");
+//const input = Deno.readTextFileSync("input-a.txt");
+//console.log(input);
+
+//
+//START YOUR SOLUTION HERE
+
+const output = "placeholder"
+//
+//OUTPUT SHOULD BE THE RESULT TO PUT ON THE ADVENT OF CODE RESPONSE FIELD
+console.log(output);
---
+++ ts/00/b.ts
@@ -0,0 +1,15 @@
+import { ensureFile } from "@std/fs";
+
+//
+//PUT YOUR INPUTS ON A VARIABLE
+await ensureFile("input.txt");
+const input = Deno.readTextFileSync("input.txt");
+//console.log(input);
+
+//
+//START YOUR SOLUTION HERE
+
+const output = "placeholder"
+//
+//OUTPUT SHOULD BE THE RESULT TO PUT ON THE ADVENT OF CODE RESPONSE FIELD
+console.log(output);
