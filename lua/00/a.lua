b378777a0e32bc96f3a3c8842142e1bcc3f5f926
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-11-29 17:54:58 -0300

    feat: lua file reads from an input

--- lua/00/a.lua
+++ lua/00/a.lua
@@ -1,2 +1,9 @@
-local message = "Hello, World!"
-print("Some message: ".. message)
+DAY="00"
+local file, err = io.open("../../inputs/" .. DAY .. "/a.txt", "r")
+
+if not file then
+  print("Error: Could not open the file. Reason: " .. (err or "unknown error"))
+  return
+end
+local message = file:read("*all")
+print(message)
