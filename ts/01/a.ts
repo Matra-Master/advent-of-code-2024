5fa251feae4f571493a0aa4e8f7b39cff34fc13d
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-12-02 14:56:49 -0300

    feat(01): solved first part
    The most horrible way possible first. Optimize Later.
    Remember the KISS principle

--- ts/01/a.ts
+++ ts/01/a.ts
@@ -4,15 +4,34 @@ const day = "01";
 //
 //PUT YOUR INPUTS ON A VARIABLE
 await ensureFile("../../inputs/" + day + "/example.txt");
-const input = Deno.readTextFileSync("../../inputs/" + day + "/example.txt");
-//const input = Deno.readTextFileSync("../../inputs/" + day + "/a.txt");
-//const input = Deno.readTextFileSync("../../inputs/" + day + "/b.txt");
-//console.log(input);
+//const input = Deno.readTextFileSync("../../inputs/" + day + "/example.txt");
+const input = Deno.readTextFileSync("../../inputs/" + day + "/a.txt");
 
 //
 //START YOUR SOLUTION HERE
+//Separate input in two lists
+const _listA :string[] = [];
+const _listB :string[] = [];
 
-const output = "placeholder"
+input.split('\n').forEach((line) => {
+  if( line === "") return
+  const split = line.split('   ');
+  _listA.push(split[0])
+  _listB.push(split[1])
+})
+_listA.sort()
+_listB.sort()
+
+console.log("List A")
+console.log(_listA)
+console.log("List B")
+console.log(_listB)
+
+const toSum = _listA.map((value,key) => {
+  return Math.abs(parseInt(_listB[key]) - parseInt(value))
+})
+
+const output = toSum.reduce((accumulator, current) => { return accumulator + current})
 //
 //OUTPUT SHOULD BE THE RESULT TO PUT ON THE ADVENT OF CODE RESPONSE FIELD
 console.log(output);
