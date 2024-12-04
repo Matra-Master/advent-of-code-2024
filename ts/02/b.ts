5461431beab92a39a5f5dde8ddc72a06197bcc5c
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-12-04 17:02:42 -0300

    feat(02): tried b and failed, it's a rewrite if a

--- ts/02/b.ts
+++ ts/02/b.ts
@@ -4,28 +4,56 @@ const day = "02";
 //
 //PUT YOUR INPUTS ON A VARIABLE
 await ensureFile("../../inputs/" + day + "/example.txt");
-const input = Deno.readTextFileSync("../../inputs/" + day + "/example.txt");
-//const input = Deno.readTextFileSync("../../inputs/" + day + "/a.txt");
+//const input = Deno.readTextFileSync("../../inputs/" + day + "/example.txt");
+const input = Deno.readTextFileSync("../../inputs/" + day + "/a.txt");
 //console.log(input);
 
 //
 //START YOUR SOLUTION HERE
-function report_is_safe(report: string): boolean {
-  let veredict = true
-  const arr = report.split(' ')
-  const examine: number[] = []
-  arr.forEach((value, index) => {
-    if (index > 0) {
-      examine[index-1] = parseInt(arr[index - 1]) - parseInt(value)
-      if (Math.abs(examine[index-1]) > 3 || Math.abs(examine[index-1]) == 0) veredict = false
-      if (index > 1)
-        if ((examine[index-2] > 0 && examine[index-1] < 0) || (examine[index-2] < 0 && examine[index-1] > 0)) veredict = false
-    }
-  })
-  console.log("Report: %s", report)
-  console.log("Examine: %s", examine)
-  return veredict
+//
+
+function examine_report(report_array: string[]): number[] {
+  const examination_array: number[] = []
+  for(const i in report_array)  {
+    const index = parseInt(i)
+    if (index > 0) examination_array.push(parseInt(report_array[index]) - parseInt(report_array[index-1]))
+  }
+  return examination_array
+
 }
+
+function is_spike(value: number) : boolean{
+  const absolute = Math.abs(value)
+  if (absolute > 3 || absolute == 0) return true
+  return false
+}
+function changes_sign(previous_value: number, value: number) : boolean{
+  if ((value > 0 && previous_value < 0) || (value < 0 && previous_value > 0)) return true
+  return false
+}
+
+
+function report_is_safe(report: string): boolean {
+  const report_array = report.split(' ')
+  const examination_array = examine_report(report_array)
+  console.log("Report array:   %s", report_array)
+  console.log("Examination array: %s", examination_array)
+
+  for (const i in examination_array) {
+    const index = parseInt(i)
+    const value = examination_array[index]
+    //if (index == examination_array.length -1) {
+    //  console.log("Got to the last value, this is safe no matter what");
+    //  break
+    //}
+    if (is_spike(value)) return false
+    if (index > 0 && changes_sign(examination_array[index-1], value)) return false
+  }
+  //console.log("returned true");
+  return true
+}
+
+
 
 const reports = input.split('\n')
 reports.pop()
@@ -33,6 +61,7 @@ reports.pop()
 let output = 0
 reports.forEach((line) => {
   if (report_is_safe(line)) output++
+  console.log("===================================")
 })
 
 //
